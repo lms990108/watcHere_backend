d6bd5fabe8cafc5e85f2e8e5ff51b0465eb25dd9
lee minseop <lms990108@gmail.com>
2023-11-15 04:31:00 +0900

    Refactor: 필요없는 import 제거

--- api/src/main/kotlin/elice/team5th/Team5thApplication.kt
+++ api/src/main/kotlin/elice/team5th/Team5thApplication.kt
@@ -1,10 +1,7 @@
 package elice.team5th
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication()
 class Team5thApplication
