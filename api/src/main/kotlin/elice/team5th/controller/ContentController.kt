2f64961176033cc7ada2d715cf0954529c8b999f
donghyun123 <numnum97@naver.com>
2023-11-17 23:20:44 +0900

    Fix: Resolve conflicts

--- api/src/main/kotlin/elice/team5th/controller/ContentController.kt
+++ api/src/main/kotlin/elice/team5th/controller/ContentController.kt
@@ -6,12 +6,7 @@ import elice.team5th.domain.content.service.ContentService
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
-
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/contents")
@@ -38,4 +33,3 @@ class ContentController(private val contentService: ContentService) {
         return ResponseEntity.ok(contents)
     }
 }
-
