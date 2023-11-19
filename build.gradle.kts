ae43f4470b7b694aa828517e1ee0bd617d83da70
lee minseop <lms990108@gmail.com>
2023-11-19 21:06:02 +0900

    Feat: api요청에 대한 tmdb응답을 구현한 상태. 테스트용으로 하나만 구성이 되어있음.

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ subprojects {
         implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
         implementation("org.springframework.boot:spring-boot-starter-security")
         implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("org.springframework.boot:spring-boot-starter-webflux")
         implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
--- core/src/main/kotlin/elice/team5th/domain/chart/model/TvingFavoriteChart.kt
+++
@@ -1,9 +0,0 @@
-package elice.team5th.domain.chart.model
-
-import elice.team5th.common.model.BaseChartEntity
-import jakarta.persistence.Entity
-import jakarta.persistence.Table
-
-@Entity
-@Table(name = "tving_favorite_chart")
-class TvingFavoriteChart : BaseChartEntity()
