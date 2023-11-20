60f75caceacb70f32131290c84c5d129679d6346
donghyun123 <numnum97@naver.com>
2023-11-20 15:25:58 +0900

    Fix: 코드 리뷰 내용 반영 - 에러 핸들러 추가 및 ErrorResponse 수정

--- core/src/main/kotlin/elice/team5th/common/exception/ErrorResponse.kt
+++ core/src/main/kotlin/elice/team5th/common/exception/ErrorResponse.kt
@@ -3,9 +3,6 @@ package elice.team5th.common.exception
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class ErrorResponse(
-    @JsonProperty("error_code")
     val errorCode: Int,
-    @JsonProperty("error_message")
     val errorMessage: String = "",
-    val detail: String = ""
 )
