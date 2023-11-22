fa7a5fddeb121c798e7d98f9e74b9ad529714ca1
lee minseop <lms990108@gmail.com>
2023-11-23 02:56:30 +0900

    Feat: 컨트롤러의 필수 파라미터가 누락될 경우 생기는 예외에 대한 핸들링

--- api/src/main/kotlin/elice/team5th/ErrorHandler.kt
+++ api/src/main/kotlin/elice/team5th/ErrorHandler.kt
@@ -2,7 +2,9 @@ package elice.team5th
 
 import elice.team5th.common.exception.EliceException
 import elice.team5th.common.exception.ErrorResponse
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MissingServletRequestParameterException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -14,5 +16,14 @@ class ErrorHandler {
             ErrorResponse(e.errorType.code, e.detail),
             e.errorType.httpStatus
         )
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException::class)
+    fun handleMissingServletRequestParameterException(e: MissingServletRequestParameterException): ResponseEntity<ErrorResponse> {
+        val errorResponse = ErrorResponse(
+            errorCode = HttpStatus.BAD_REQUEST.value(), // 'errorCode' 파라미터를 제공해야 함
+            errorMessage = "The required parameter '${e.parameterName}' is missing."
+        )
+        return ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST)
     }
 }
