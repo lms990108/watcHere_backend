6c62bcf2ec5b9a8a5e838724d2b567e80a51c171
donghyun123 <numnum97@naver.com>
2023-11-22 20:00:52 +0900

    Fix: server 분리 설정 추가

--- api/src/main/kotlin/elice/team5th/config/SwaggerConfig.kt
+++ api/src/main/kotlin/elice/team5th/config/SwaggerConfig.kt
@@ -1,7 +1,9 @@
 package elice.team5th.config
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -9,11 +11,19 @@ import org.springframework.context.annotation.Configuration
 class SwaggerConfig {
     @Bean
     fun openAPI(): OpenAPI {
-        return OpenAPI().info(
+        val devServer = Server()
+            .url("http://kdt-sw-6-team05.elicecoding.com")
+            .description("Dev Server")
+        val localServer = Server()
+            .url("http://localhost:8080")
+            .description("Local Server")
+        return OpenAPI()
+            .info(
             Info()
                 .title("Elice 5th API")
                 .description("Elice 5th API Docs")
                 .version("v1.0.0")
         )
+            .servers(listOf(devServer, localServer))
     }
 }
