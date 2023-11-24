07c4912c58b75ece36b98ba2d09effc26b85e371
lee minseop <lms990108@gmail.com>
2023-11-24 18:43:45 +0900

    Feat: tv, movie, genre, video 엔티티 생성

---
+++ core/src/main/kotlin/elice/team5th/domain/tmdb/entity/VideoEntity.kt
@@ -0,0 +1,35 @@
+package elice.team5th.domain.tmdb.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "videos")
+class VideoEntity(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id", nullable = true)
+    val movie: MovieEntity? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tvshow_id", nullable = true)
+    val tvShow: TVShowEntity? = null,
+
+    @Column(name = "video_key")
+    val key: String,
+
+    val name: String,
+    val site: String,
+    val type: String,
+)
