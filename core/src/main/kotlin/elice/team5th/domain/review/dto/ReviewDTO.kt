4a1dd8a541e3c7cbb66b3fdda6951e4506fefb54
donghyun123 <numnum97@naver.com>
2023-11-28 01:04:07 +0900

    Fix: 유저 리뷰 연관관계 정리(진짜 완)

--- core/src/main/kotlin/elice/team5th/domain/review/dto/ReviewDTO.kt
+++ core/src/main/kotlin/elice/team5th/domain/review/dto/ReviewDTO.kt
@@ -1,15 +1,27 @@
 package elice.team5th.domain.review.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import elice.team5th.domain.review.model.Review
+import elice.team5th.domain.user.dto.UserDto
 import elice.team5th.domain.user.model.User
 
 data class ReviewDTO(
     val id: Long? = null,
-    val userId: String,
+    val author: UserDto.Response,
     @JsonProperty("content_id")
     val contentId: Int,
     val detail: String,
     val rating: Int,
     val likes: Int,
     val reports: Int
-)
+) {
+    constructor(review: Review) : this(
+        id = review.id,
+        author = UserDto.Response(review.user),
+        contentId = review.contentId,
+        detail = review.detail,
+        rating = review.rating,
+        likes = review.likes,
+        reports = review.reports
+    )
+}
