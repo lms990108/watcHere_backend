cdaea35cf29f17d674eea2276b42d024ad5a27a8
donghyun123 <numnum97@naver.com>
2023-12-01 23:52:58 +0900

    Feat: 유저 서비스에 대한 단위 테스트 작성(미완)

---
+++ core/src/test/kotlin/elice/team5th/Team5thApplicationTest.kt
@@ -0,0 +1,13 @@
+package elice.team5th
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Team5thApplicationTest {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
